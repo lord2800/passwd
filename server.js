6cc8baca4a9c94f670d436ec7eb898a038cd24c2
Jeff Hubbard <lord2800@gmail.com>
2017-08-18 18:46:52 -0700

    Just make the whole site a static site, no server required

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-	morgan = require('morgan');
-
-var app = module.exports = express();
-app.use(morgan('dev'));
-app.use(express.static('public'));
-
-app.listen(process.env.PORT || 3000);
